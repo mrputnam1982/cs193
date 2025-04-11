64255c5404b13f30f0d53975ba892b7f433c401b
mrputnam1982 <mike.putnam@gmail.com>
2025-04-11 17:35:13 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Mike Putnam on 4/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
