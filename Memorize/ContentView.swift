b19382d39954b3827911607642f5253edcf40ab0
mrputnam1982 <mike.putnam@gmail.com>
2025-04-11 18:03:35 -0400

    Added tappable gesture to cards

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,16 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
         }
         .padding()
+        .foregroundColor(.orange)
     }
 }
 
+struct CardView : View {
+    @State var isFaceUp = false
+    var body: some View{
+        ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
+            if(isFaceUp){
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
+                Text("👻").font(.largeTitle)
+            }else {
+                base.fill()
+                
+            }
+        }.onTapGesture {
+            isFaceUp.toggle();
+        }
+    }
+}
 #Preview {
     ContentView()
 }
